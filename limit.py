50bb883555ee0848382bf70b18c8cb9f99aa321b
Krai53n <greasha46@gmail.com>
2021-03-20 15:00:49 +0300

    (¯▿¯)

--- limit.py
+++ limit.py
@@ -4,7 +4,7 @@ from os import walk
 from os import getcwd
 import os.path
 
-from yaml import load
+from yaml import safe_load
 from yaml import dump
 
 
@@ -20,7 +20,7 @@ def create_file(data, filename):
 
 def open_file(filename):
     with open(filename, "r") as f:
-        date = load(f)
+        date = safe_load(f)
     return date
 
 def matrix_to_list(matrix):
@@ -29,26 +29,88 @@ def matrix_to_list(matrix):
         lst.extend(i)
     return lst
 
-class Cli:
-    def __init__(self):
-        data = self.__process_file()
-        if data:
-            print("I have data")
-        if not data:
-            print("I create data")
 
-    def __parse_yaml_extensions(self):
+class Yaml:
+    def __parse_files(self, path):
+        """
+        Arguments:
+            path - path to directory
+        """
         # names of dirs that you want to skip
         skip_dir = (
             ".git",
         )
         paths = []
+        for root, dirs, files in walk(path):
+            skip = False
+            for skip_item in skip_dir:
+                if skip_item in root[len(path):]:
+                    skip = True
+            if not skip:
+                for fl in files:
+                    paths.append(os.path.join(root, fl))
         return paths
+
+    def parse_yaml_extensions(self, path):
+        """
+        Arguments:
+            path - path to directory
+        """
+        ext = ".yaml"
+        files = self.__parse_files(path)
+        files = [i for i in files if i[len(i)-len(ext):] == ext]
+        return files
+
+
+class Cli:
+    def __init__(self):
+        data = self.__process_file()
+        if data:
+            print(data)
+        if not data:
+            print("I create data")
+
+    def __parse_files(self, path):
+        """
+        Arguments:
+            path - path to directory
+        """
+        # names of dirs that you want to skip
+        skip_dir = (
+            ".git",
+        )
+        paths = []
+        for root, dirs, files in walk(path):
+            skip = False
+            for skip_item in skip_dir:
+                if skip_item in root[len(path):]:
+                    skip = True
+            if not skip:
+                for fl in files:
+                    paths.append(os.path.join(root, fl))
+        return paths
+
+    def __parse_yaml_extensions(self, path):
+        """
+        Arguments:
+            path - path to directory
+        """
+        ext = ".yaml"
+        files = self.__parse_files(path)
+        files = [i for i in files if i[len(i)-len(ext):] == ext]
+        return files
 
     def __process_file(self):
         try:
-            #self.__parse_yaml_extensions()
-            return open_file("sdfg")
+            files = self.__parse_yaml_extensions(getcwd())
+            filename = ""
+            if len(files) == 1:
+                filename = files[0]
+            if len(files) > 1:
+                print("Choose enter:")
+                [print("\t".format(i+1, files[i])) for i in range(len(files))]
+                filename = files[int(input(print("Your enter: "))) - 1]
+            return open_file(filename)
         except FileNotFoundError:
             self.__create_file()
 
